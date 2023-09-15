6b575156ac7f919c07c515abd2f19745a746ea07
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-09-15 22:12:24 +0600

    using for loop for total price

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,25 +2,25 @@
 
 
 // eslint-disable-next-line react/prop-types
-const Cart = ({ selectedCourses, remaining, totalCredit }) => {
+const Cart = ({ selectedCourses, remaining, totalCredit, totalCost }) => {
     console.log(selectedCourses)
     return (
         <div>
                 <h1 className="font-semibold text-blue-500 pb-2">Credit Hour Remaining: {remaining} hr</h1>
                 <hr />
                 <p className="font-bold pb-2">Course Name</p>
-                {/* mapping */}
-               
+                {/* mapping */}  
+                       
                 {selectedCourses.map((course) =>(           
-                   <ol  key={course.id}>
-                     <li className="text-lg text-gray-500 pb-2">{course.title}</li>
-                   </ol>
+                   
+                     <li key={course.id} className="list-decimal text-lg text-gray-500 pb-2">{course.title}</li>
+                   
                 ))}
 
                 <hr />
-                <p className="text-xl pb-2">Total Credit Hour: {totalCredit} hr</p>
+                <p className="text-xl pb-2">Total Credit Hour: {totalCredit} </p>
                 <hr />
-                <p className="text-xl pb-2">Total Price:</p>
+                <p className="text-xl pb-2">Total Price: {totalCost} USD</p>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import Cart from "../Cart/Cart";
-// for toast pakeage
+// for toast
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+// for icon
+
 
 
 const Home = () => {
-    // store the data
+    // store the data by using useState
     const [allCourses, setAllCourses] = useState([]);
     const [selectedCourses, setSelectedCourses] = useState([]);
     const [remaining, setRemaining] = useState(0);
     const [totalCredit, setTotalCredit] = useState(0);
+    const [totalCost, setTotalCost] = useState(0)
     // fetching the data
     useEffect(() => {
         fetch('./data.json')
@@ -20,17 +23,23 @@ const Home = () => {
     },[]);
 
     const handleSelectCourse = (course) =>{
-        const isExist = selectedCourses.find(item => item.id === course.id);
+        const isExist = selectedCourses.find((item) => item.id === course.id);
 
         let count =course.credit;
+        let cost = course.price;
 
         if(isExist){
-            toast('This course is already taken. Select other courses')
+            toast('This course is already taken. Select other courses');
         }
+
         else{
-            // for each loop
-            selectedCourses.forEach(item =>{
+            // for each loop for credit
+            selectedCourses.forEach((item) =>{
                 count += item.credit;
+            });
+            // for each loop for price
+            selectedCourses.forEach((item) =>{
+                cost += item.price;
             });
             // for remaining value
             const totalRemaining = 20 - count;
@@ -41,9 +50,9 @@ const Home = () => {
             }
             else{
                 setTotalCredit(count);
-                setRemaining(totalRemaining)
-                
-                setSelectedCourses([...selectedCourses, course])
+                setRemaining(totalRemaining);
+                setTotalCost(cost);
+                setSelectedCourses([...selectedCourses, course]);
             }
         }
     };
@@ -52,7 +61,7 @@ const Home = () => {
     return (
         <div className="container mx-auto">
             <h1 className='mt-10 text-4xl font-bold text-center '>Course Registration</h1> 
-        <div className="lg:flex">
+        <div className="flex flex-col-reverse lg:flex-row">
             {/* card design */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 md:ml-6">
         {/* mapping card */}
@@ -61,10 +70,16 @@ const Home = () => {
                 <div key={course.id} className="card w-80 bg-base-100 shadow-2xl rounded-lg p-3">
                 <img className="w-80 items-center rounded-xl py-2" src={course.cover} alt="Shoes" />
                 <h2 className="text-xl font-semibold py-2 text-center">{course.title}</h2>
-                <p>{course.details}</p>
-                    <div className="flex justify-around py-2">
-                    <p>Price: {course.price}</p>
+                <p className="text-gray-500 pb-3">{course.details}</p>
+                    <div className="flex justify-between py-2">
+                    <div className="flex gap-2">
+                    <img className="w-5" src="https://i.ibb.co/tKhyr7J/dollar-sign-1.jpg" alt="" />
+                    <p> Price: {course.price}</p>
+                    </div>
+                    <div className="flex gap-2">
+                    <img className="w-5" src="https://i.ibb.co/0tYRcVB/Frame.jpg" alt="" />
                     <p>Credit: {course.credit} hr</p>
+                    </div>
                     </div>
                     <div>
                         <button onClick={()=>handleSelectCourse(course)} className="bg-blue-600 w-72 items-center p-2 rounded-xl ml-1 my-2 text-white">Select</button>
@@ -91,7 +106,8 @@ const Home = () => {
                 <Cart 
                 selectedCourses={selectedCourses}
                 remaining = {remaining}
-                totalCredit = {totalCredit}>                  
+                totalCredit = {totalCredit}
+                totalCost = {totalCost}>                  
                 </Cart>
             </div>
         </div>
